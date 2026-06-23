599d2168bde49f5d580713ef34e26e6877df00df
phoebe-jane garcia <phoebegarcia@protonmail.com>
2026-06-23 00:11:09 -0400

    Create nutrition_db.py

---
+++ app/domain/nutrition_db.py
@@ -0,0 +1,30 @@
+MACROS = {
+
+    "banana": {
+        "calories":105,
+        "protein":1.3,
+        "carbs":27,
+        "fat":0.4
+    },
+
+    "chicken breast": {
+        "calories":165,
+        "protein":31,
+        "carbs":0,
+        "fat":3.6
+    },
+
+    "rice": {
+        "calories":205,
+        "protein":4,
+        "carbs":45,
+        "fat":0.4
+    },
+
+    "olive oil": {
+        "calories":119,
+        "protein":0,
+        "carbs":0,
+        "fat":14
+    }
+}
